ebd5650adf1f5919142fa2731f3ddc02890a1dac
Himanshu Singh <96868684+HimySingh@users.noreply.github.com>
2022-05-31 18:01:58 +0530

    Add files via upload

---
+++ Pattern Formation.py
@@ -0,0 +1,16 @@
+for i in range(4):
+    for j in range(0,i+1):
+        print("Him", end=" ")
+    print("\n")
+
+
+for i in range(4):
+    for j in range(0,i+1):
+        print("Him", end=" ")
+    print(" ")
+
+
+for i in range(4):
+    for j in range(0,i+1):
+        print("Him", end=" ")
+    print()
